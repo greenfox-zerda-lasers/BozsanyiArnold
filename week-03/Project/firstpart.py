37e278936ee73aceaaa47c21de1ea72aa6015710
Bozsanyi Arnold <bozsanyi.arnold0720@gmail.com>
2016-12-05 16:35:18 +0100

    week-03 first part of weekly project modified

--- week-03/Project/firstpart.py
+++ week-03/Project/firstpart.py
@@ -1,16 +1,13 @@
 def reverser(string):
-        revers = ''
-        i = 0
-        while i < len(string):
-            revers += string[(len(string)-1)-i]
-            i += 1
-        return revers
+    revers = ''
+    i = 0
+    while i < len(string):
+        revers += string[(len(string)-1)-i]
+        i += 1
+    return revers
 
-def crtpalindrome(string):
+def create_palindrome(string):
     palindrome = string + reverser(string)
     return palindrome
 
-print(crtpalindrome('paer'))
-
-def if_palindrome(string):
-    
+print(create_palindrome('pear'))
