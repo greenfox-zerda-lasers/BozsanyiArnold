2d8fb6ccc50d6b74e6fcd8122d857af8b3f9c720
Bozsanyi Arnold <bozsanyi.arnold0720@gmail.com>
2016-12-21 13:28:04 +0100

    get byID and post method added

--- week-09/TheTODO/server.js
+++ week-09/TheTODO/server.js
@@ -2,15 +2,41 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Kutule0915'
+})
+
 var app = express();
 
 app.use(bodyParser.json());
 
 app.get('/todos', function getTodos(req, res) {
-  connection.query('Select * FROM The_Todos', function(error, rows) {
+  connection.query('SELECT * FROM The_Todos', function(error, rows) {
     if (error) {
-      console.log(error.toString());
+      res.send(error.toString());
     }
     res.send(rows);
+  });
+});
+
+app.get('/todos/:id', function getTodoById(req, res) {
+  connection.query('SELECT * FROM The_Todos WHERE id = ' + req.params.id + ';', function(error, row) {
+    if (error) {
+      res.send(error.toString());
+    }
+    res.send(row);
+  });
+});
+
+app.post('/todos', function addATodo(req, res) {
+  connection.query('INSERT INTO The_Todos VALUES ('+ req.body.text + ')', function(error, row) {
+    if (error) {
+      res.send(error.toString());
+    }
+    res.send(row)
   });
 });
